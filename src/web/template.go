00d8605612b73fb7e77ae8c24fab90314ff22cb8
Raw Monk <realmelkor@proton.me>
2023-09-28 20:42:21 -0400

    format post with greentext and links

--- src/web/template.go
+++ src/web/template.go
@@ -5,6 +5,7 @@ import (
 	"html/template"
 	"bytes"
 	"strings"
+	"strconv"
 
 	"IB1/db"
 )
@@ -101,8 +102,57 @@ func renderThread(thread db.Thread) (string, error) {
 	return buf.String(), nil
 }
 
+func addLinks(content string) string {
+	const quote = "&gt;&gt;"
+	for i := strings.Index(content, quote); i >= 0 &&
+			i + len(quote) < len(content); {
+
+		index := strings.Index(content[i:], quote)
+		if index < 0 { break }
+		i += index + len(quote)
+
+		j := i
+		length := len(content)
+		for ; j < length; j++ {
+			if (content[j] < '0' || content[j] > '9') { break }
+		}
+		if (j >= length && content[j] != ' ' && content[j] != '\n' &&
+				content[j] == '\t') {
+			continue
+		}
+		number := content[i:j]
+		_, err := strconv.Atoi(number)
+		if err != nil { continue }
+		str := "<a href=\"#" + number + "\">&gt;&gt;" + number + "</a>"
+		content = content[:i - len(quote)] + str + content[j:]
+		i += len(str) - len(quote)
+	}
+	return content
+}
+
+func addGreentext(content string) string {
+	const br = "<br>"
+	strings.ReplaceAll(content, "\r", "")
+	length := len(content)
+	next := 0
+	for i := 0; i >= 0 && i < length; i = next {
+		next = strings.Index(content[i:], br)
+		if next == -1 { next = length } else { next += i + len(br) }
+		if strings.Index(content[i:next], "&gt;&gt;") == 0 { continue }
+		if strings.Index(content[i:next], "&gt;") != 0 { continue }
+		line := "<span class=\"green-text\">" +
+				content[i:next] + "</span>"
+		content = content[:i] + line + content[next:]
+		length = len(content)
+		next = i + len(line)
+	}
+	return content
+}
+
 func parseContent(content string) template.HTML {
 	content = template.HTMLEscapeString(content)
 	content = strings.Replace(content, "\n", "<br>", -1)
+	content = addLinks(content)
+	content = addGreentext(content)
 	return template.HTML(content)
 }
