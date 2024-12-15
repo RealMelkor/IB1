f84eee1872625f550da589c5e98e91d0089cb79e
Raw Monk <rawmonk@rmf-dev.com>
2024-12-14 19:49:41 -0500

    use standard base64 for tokens

--- src/web/token.go
+++ src/web/token.go
@@ -1,21 +1,15 @@
 package web
 
 import (
-        "crypto/rand"
+	"crypto/rand"
+	"encoding/base64"
 )
 
-const characters =
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@"
-const charactersLength = byte(len(characters) - 1)
-const tokenLength = byte(32)
+const tokenLength = 32
 
 func newToken() (string, error) {
-        var random [tokenLength]byte
-        var b [tokenLength]byte
-        _, err := rand.Read(random[:])
-        if err != nil { return "", err }
-        for i := range b {
-                b[i] = characters[random[i] & charactersLength]
-        }
-        return string(b[:]), nil
+	var random [tokenLength]byte
+	_, err := rand.Read(random[:])
+	if err != nil { return "", err }
+	return base64.RawStdEncoding.EncodeToString(random[:]), nil
 }
